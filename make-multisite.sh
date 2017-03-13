05159b461e326657f15a588b7cb1a3dd0dfca4a7
Avi Alkalay <avibrazil@gmail.com>
2017-03-12 23:36:58 -0300

    instrument after import

--- make-multisite.sh
+++ make-multisite.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 
-DOMAIN="avi.alkalay.net"
 MULTISITE_ID=2
 
-OLD_HOST="mysql.alkalay.net"
-OLD_USER="blog"
+OLD_DOMAIN="avi.alkalay.net"
+OLD_DB_HOST="mysql.alkalay.net"
+OLD_DB_USER="blog"
 OLD_DB="blog_57"
 OLD_TABLE_PREFIX="wp_"
 
-NEW_HOST=$OLD_HOST
-NEW_USER=$OLD_USER
+NEW_DOMAIN="test.alkalay.net"
+NEW_DB_HOST=$OLD_HOST
+NEW_DB_USER=$OLD_USER
 NEW_DB="blog2"
 NEW_TABLE_PREFIX=$OLD_TABLE_PREFIX
 
 echo
 echo "Export and convert..."
-mysqldump -h "$OLD_HOST" -u "$OLD_USER" -p --add-drop-table $OLD_DB |
+mysqldump -h "$OLD_DB_HOST" -u "$OLD_DB_USER" -p --add-drop-table "$OLD_DB" |
 
 sed -e "
 	s|^DROP TABLE IF EXISTS \`${OLD_TABLE_PREFIX}|DROP TABLE IF EXISTS \`${NEW_TABLE_PREFIX}${MULTISITE_ID}_|g;
@@ -24,12 +25,18 @@ sed -e "
 	s|^INSERT INTO \`${OLD_TABLE_PREFIX}|INSERT INTO \`${NEW_TABLE_PREFIX}${MULTISITE_ID}_|g;
 	s|ALTER TABLE \`${OLD_TABLE_PREFIX}|ALTER TABLE \`${NEW_TABLE_PREFIX}${MULTISITE_ID}_|g;
 	s|${OLD_TABLE_PREFIX}user_roles|${NEW_TABLE_PREFIX}${MULTISITE_ID}_user_roles|g;
-" | gzip -c -9 > "$DOMAIN".multisite.sql.gz
+" | gzip -c -9 > "$NEW_DOMAIN".multisite.sql.gz
 
 
 echo
 echo "Import converted..."
-zcat "$DOMAIN".multisite.sql.gz | mysql -h "$NEW_HOST" -u "$NEW_USER" -p "$NEW_DB"
+zcat "$NEW_DOMAIN".multisite.sql.gz | mysql -h "$NEW_DB_HOST" -u "$NEW_DB_USER" -p "$NEW_DB"
 
 
+echo
+echo "Last instrumentations..."
 
+mysql -h "$NEW_DB_HOST" -u "$NEW_DB_USER" -p "$NEW_DB" <<EndOfInstrumentation
+UPDATE `${NEW_TABLE_PREFIX}${MULTISITE_ID}_options` SET `option_value` = 'https://${NEW_DOMAIN}' WHERE `wp_2_options`.`option_name` = 'home';
+UPDATE `${NEW_TABLE_PREFIX}${MULTISITE_ID}_options` SET `option_value` = 'https://${NEW_DOMAIN}' WHERE `wp_2_options`.`option_name` = 'siteurl';
+EndOfInstrumentation
