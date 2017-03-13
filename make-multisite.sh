13c00ad6c9ca071fb967bc2d6af7c621d5fc7598
Avi Alkalay <avibrazil@gmail.com>
2017-03-12 23:09:39 -0300

    .

--- make-multisite.sh
+++ make-multisite.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
-# mysqldump -h 'mysql.alkalay.net' -u blog -p --add-drop-table blog_57
-
-OLD_TABLE_PREFIX="wp_"
-NEW_TABLE_PREFIX="wp_"
-
+DOMAIN="avi.alkalay.net"
 MULTISITE_ID=2
+
+OLD_HOST="mysql.alkalay.net"
+OLD_USER="blog"
+OLD_DB="blog_57"
+OLD_TABLE_PREFIX="wp_"
+
+NEW_HOST=$OLD_HOST
+NEW_USER=$OLD_USER
+NEW_DB=$OLD_DB
+NEW_TABLE_PREFIX=$NEW_TABLE_PREFIX
+
+
+
+mysqldump -h "$OLD_HOST" -u "$OLD_USER" -p --add-drop-table $OLD_DB |
 
 sed -e "
 	s|^DROP TABLE IF EXISTS \`${OLD_TABLE_PREFIX}|DROP TABLE IF EXISTS \`${NEW_TABLE_PREFIX}${MULTISITE_ID}_|g;
@@ -14,6 +24,10 @@ sed -e "
 	s|^INSERT INTO \`${OLD_TABLE_PREFIX}|INSERT INTO \`${NEW_TABLE_PREFIX}${MULTISITE_ID}_|g;
 	s|ALTER TABLE \`${OLD_TABLE_PREFIX}|ALTER TABLE \`${NEW_TABLE_PREFIX}${MULTISITE_ID}_|g;
 	s|${OLD_TABLE_PREFIX}user_roles|${NEW_TABLE_PREFIX}${MULTISITE_ID}_user_roles|g;
-"	
+" | gzip -c -9 > "$DOMAIN".multisite.sql.gz
+
+
+zcat "$DOMAIN".multisite.sql.gz | mysql -h "$NEW_HOST" -u "$NEW_USER" -p "$NEW_DB"
+
 
 
