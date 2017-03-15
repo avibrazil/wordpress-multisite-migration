bbff206c034811f8dd6fd6aaffdb49f62befd7a1
Avi אבי Alkalay אלקלעי <avibrazil@gmail.com>
2017-03-15 06:06:55 -0300

    last documentations

--- make-multisite.sh
+++ make-multisite.sh
@@ -16,6 +16,8 @@ NEW_TABLE_PREFIX=$OLD_TABLE_PREFIX
 
 echo
 echo "Export and convert..."
+echo "I'll need password for the OLD database now..."
+
 mysqldump -h "$OLD_DB_HOST" -u "$OLD_DB_USER" -p --add-drop-table "$OLD_DB" |
 
 sed -e "
@@ -30,17 +32,22 @@ sed -e "
 
 echo
 echo "Import converted..."
+echo "I'll need password for the NEW database now..."
+
 zcat "$NEW_DOMAIN".multisite.sql.gz | mysql -h "$NEW_DB_HOST" -u "$NEW_DB_USER" -p "$NEW_DB"
 
 
 echo
 echo "Last instrumentations..."
+echo "I'll need password for the NEW database now, again..."
 
 mysql -h "$NEW_DB_HOST" -u "$NEW_DB_USER" -p "$NEW_DB" <<EndOfInstrumentation
 UPDATE ${NEW_TABLE_PREFIX}${MULTISITE_ID}_options SET option_value = 'https://${NEW_DOMAIN}' WHERE ${NEW_TABLE_PREFIX}${MULTISITE_ID}_options.option_name = 'home';
 UPDATE ${NEW_TABLE_PREFIX}${MULTISITE_ID}_options SET option_value = 'https://${NEW_DOMAIN}' WHERE ${NEW_TABLE_PREFIX}${MULTISITE_ID}_options.option_name = 'siteurl';
 UPDATE ${NEW_TABLE_PREFIX}${MULTISITE_ID}_options SET option_value = 'https://${NEW_DOMAIN}' WHERE ${NEW_TABLE_PREFIX}${MULTISITE_ID}_options.option_name = 'oid_trust_root';
 EndOfInstrumentation
+
+echo "End of multisite conversion and migration."
 
 # Later, I found this as a nice writeup about DB migration:
 # https://deliciousbrains.com/wp-migrate-db-pro/doc/exporting-single-site-as-subsite-for-multisite-install/
